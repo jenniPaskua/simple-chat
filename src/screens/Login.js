362911ff17c6b0c3bdacd9db533222272366f74c
조지은 <jenniPaskua@gmail.com>
2022-05-17 20:10:33 +0900

    ✨email validation - regular expression

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,16 @@ import styled from "styled-components/native";
 import { Image, Input } from "../components";
 import { images } from "../utils/images";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { validateEmail, removeWhitespace } from "../utils/common";
+
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 10px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.errorText};
+`;
 
 const Container = styled.View`
   flex: 1;
@@ -16,18 +26,31 @@ const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const passwordRef = useRef();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const _handleEmailChange = (email) => {
+    const changedEmail = removeWhitespace(email);
+    setEmail(changedEmail);
+    setErrorMessage(
+      validateEmail(changedEmail) ? "" : "Please verify your email"
+    );
+  };
+
+  const _handlePasswordChange = (password) => {
+    setPassword(removeWhitespace(password));
+  };
 
   return (
     <KeyboardAwareScrollView
       contentContainerStyle={{ flex: 1 }}
-      extraScrollHeight={20}
+      extraScrollHeight={30}
     >
       <Container>
         <Image url={images.icon} />
         <Input
           label='Email'
           value={email}
-          onChangeText={(text) => setEmail(text)}
+          onChangeText={_handleEmailChange}
           onSubmitEditing={() => passwordRef.current.focus()}
           placeholder='Email'
           returnKeyType='next'
@@ -36,12 +59,13 @@ const Login = ({ navigation }) => {
           ref={passwordRef}
           label='Password'
           value={password}
-          onChangeText={(text) => setPassword(text)}
+          onChangeText={_handlePasswordChange}
           onSubmitEditing={() => {}}
           placeholder='Password'
           returnKeyType='done'
           isPassword
         />
+        <ErrorText>{errorMessage}</ErrorText>
       </Container>
     </KeyboardAwareScrollView>
   );
