354be58b3e7d4bf4af6f5429a1b763bbb1474ac4
조지은 <jenniPaskua@gmail.com>
2022-05-18 09:20:39 +0900

    ✨ firebase signup using auth

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,6 +5,8 @@ import { images } from "../utils/images";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { validateEmail, removeWhitespace } from "../utils/common";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Alert } from "react-native";
+import { signin } from "../utils/firebase";
 
 const ErrorText = styled.Text`
   align-items: flex-start;
@@ -50,7 +52,14 @@ const Login = ({ navigation }) => {
     setPassword(removeWhitespace(password));
   };
 
-  const _handleLoginButtonPress = () => {};
+  const _handleLoginButtonPress = async () => {
+    try {
+      const user = await signin({ email, password });
+      Alert.alert("Login Success", user.email);
+    } catch (e) {
+      Alert.alert("Login Error", e.message);
+    }
+  };
 
   return (
     <KeyboardAwareScrollView
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,13 @@
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
 import config from "../../firebase.json";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const app = firebase.initializeApp(config);
+const app = initializeApp(config);
+
+const auth = getAuth(app);
+
+export const signin = async ({ email, password }) => {
+  //   const { user } = await Auth.signInWithEmailAndPassword(email, password);
+  const { user } = await signInWithEmailAndPassword(auth, email, password);
+  return user;
+};
